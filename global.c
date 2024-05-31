f5c30f8d3d4eab7eb7ebc0522a46e068722e297f
Newpage927 <newpage927@gmail.com>
2024-06-01 04:24:32 +0800

    Update global.c

--- global.c
+++ global.c
@@ -1,16 +1,32 @@
-#include "global.h"
-#include "shapes/Shape.h"
-#include <stdbool.h>
-// variables for global usage
-const double FPS = 60.0;
-const int WIDTH = 1250;
-const int HEIGHT = 500;
-int window = 0;
-int window2 = 0;
-ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-ALLEGRO_EVENT event;
-ALLEGRO_TIMER *fps = NULL;
-bool key_state[ALLEGRO_KEY_MAX] = {false};
-bool mouse_state[ALLEGRO_MOUSE_MAX_EXTRA_AXES] = {false};
-Point mouse;
-bool debug_mode = true;
+#ifndef GLOBAL_H_INCLUDED
+#define GLOBAL_H_INCLUDED
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#define GAME_TERMINATE -1
+#include "shapes/Point.h"
+#define debug(x)                                                            \
+    if (x)                                                                  \
+    {                                                                       \
+        printf("check at line number %d in file %s\n", __LINE__, __FILE__); \
+    }
+#define MAX_ELEMENT 100
+#define min(x, y) ((x) > (y) ? (y) : (x))
+#define max(x, y) ((x) < (y) ? (y) : (x))
+// note that you can't assign initial value for extern variable!
+extern const double FPS;
+extern const int WIDTH;
+extern const int HEIGHT;
+extern int window;
+extern int window2;
+extern int a;
+extern int delay;
+extern bool key_state[ALLEGRO_KEY_MAX];
+extern bool prev_key_state[ALLEGRO_KEY_MAX];
+extern bool mouse_state[ALLEGRO_MOUSE_MAX_EXTRA_AXES];
+extern Point mouse;
+extern ALLEGRO_EVENT_QUEUE *event_queue;
+extern ALLEGRO_EVENT event;
+extern ALLEGRO_TIMER *fps;
+extern bool debug_mode;
+
+#endif
