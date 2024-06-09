2b23ef9a48e376273f1c1d82afa2226288247658
Newpage927 <newpage927@gmail.com>
2024-06-10 04:19:47 +0800

    Update menu.c

--- scene/menu.c
+++ scene/menu.c
@@ -26,12 +26,53 @@ Scene *New_Menu(int label)
     else if(window2==5){
         pDerivedObj->background = al_load_bitmap("assets/image/op4.jpg");
     }
-
+    else if(window2==6){
+        pDerivedObj->background = al_load_bitmap("assets/image/dead.jpg");
+    }
+    else if(window2==7){
+        pDerivedObj->background = al_load_bitmap("assets/image/about.jpg");
+    }
+    else if(window2==8){
+        pDerivedObj->background = al_load_bitmap("assets/image/end.jpg");
+    }
+    else if(window2==9){
+        pDerivedObj->background = al_load_bitmap("assets/image/end1.jpg");
+    }
+    else if(window2==10){
+        pDerivedObj->background = al_load_bitmap("assets/image/end2.jpg");
+    }
+    else if(window2==11){
+        pDerivedObj->background = al_load_bitmap("assets/image/end3.jpg");
+    }
+    else if(window2==12){
+        pDerivedObj->background = al_load_bitmap("assets/image/end4.jpg");
+    }
+    else if(window2==13){
+        pDerivedObj->background = al_load_bitmap("assets/image/end5.jpg");
+    }
+    else if(window2==14){
+        pDerivedObj->background = al_load_bitmap("assets/image/end6.jpg");
+    }
+    else if(window2==15){
+        pDerivedObj->background = al_load_bitmap("assets/image/end7.jpg");
+    }
+    else if(window2==16){
+        pDerivedObj->background = al_load_bitmap("assets/image/end8.jpg");
+    }
+    else if(window2==17){
+        pDerivedObj->background = al_load_bitmap("assets/image/end9.jpg");
+    }
+    else if(window2==18){
+        pDerivedObj->background = al_load_bitmap("assets/image/end10.jpg");
+    }
+    else if(window2==19){
+        pDerivedObj->background = al_load_bitmap("assets/image/end11.jpg");
+    }
     Scene *pObj = New_Scene(label);
     // setting derived object member
     //pDerivedObj->font = al_load_ttf_font("assets/font/pirulen.ttf", 12, 0);
     // Load sound
-    pDerivedObj->song = al_load_sample("assets/sound/bgm1.mp3");
+    pDerivedObj->song = al_load_sample("assets/sound/bgm2.mp3");
     al_reserve_samples(20);
     pDerivedObj->sample_instance = al_create_sample_instance(pDerivedObj->song);
     pDerivedObj->title_x = WIDTH / 2;
@@ -69,6 +110,11 @@ void menu_update(Scene *self)
             self->scene_end = true;
             window=1;
         }
+        if (key_state[ALLEGRO_KEY_A])
+        {
+            window2=7;
+            create_scene(Menu_L);
+        }
     }
     if(window2==2){
         if (prev_key_state[ALLEGRO_KEY_L]==0&&key_state[ALLEGRO_KEY_L]==1)
@@ -77,6 +123,7 @@ void menu_update(Scene *self)
             create_scene(Menu_L);
             key_state[ALLEGRO_KEY_L]=prev_key_state[ALLEGRO_KEY_L];
         }
+        
     }
     if(window2==3){
         if (prev_key_state[ALLEGRO_KEY_L]==0&&key_state[ALLEGRO_KEY_L]==1)
@@ -100,6 +147,117 @@ void menu_update(Scene *self)
             window=1;
         }
     }
+    if(window2==6){
+        if (key_state[ALLEGRO_KEY_M])
+        {
+            window2=1;
+            pass=0;
+            create_scene(Menu_L);
+        }
+    }
+    if(window2==7){
+        if (key_state[ALLEGRO_KEY_M])
+        {
+            window2=1;
+            create_scene(Menu_L);
+        }
+    }
+    if(window2==8){
+        count--;
+        if(count==940){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    if(window2==9){
+        count--;
+        if(count==880){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    if(window2==10){
+        count--;
+        if(count==820){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    if(window2==11){
+        count--;
+        if(count==760){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    if(window2==12){
+        count--;
+        if(count==700){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    if(window2==13){
+        count--;
+        if(count==670){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    if(window2==14){
+        count--;
+        if(count==640){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    if(window2==15){
+        count--;
+        if(count==610){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    if(window2==16){
+        count--;
+        if(count==580){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    if(window2==17){
+        count--;
+        if(count==550){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    if(window2==18){
+        count--;
+        if(count==490){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    if(window2==19){
+        if(count==430){
+            window2++;
+            create_scene(Menu_L);
+            key_state[ALLEGRO_KEY_ENTER]=prev_key_state[ALLEGRO_KEY_ENTER];
+        }
+    }
+    
     /*if (prev_key_state[ALLEGRO_KEY_SPACE]==0&&key_state[ALLEGRO_KEY_SPACE]==1)
         {
             window2++;
